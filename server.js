20f76ca7942e34cadde1e20036b5086a488ea787
Nishant Sharma <nishusharma1817@gmail.com>
2025-04-10 04:54:13 +0530

    delted comments

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Allow dynamic frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"], // Explicitly allow methods
-  credentials: true, // If you use cookies or auth headers
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  credentials: true, 
 }));
 
 
