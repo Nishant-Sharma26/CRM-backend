bff5ad2d76b191dee0b1f43505864bd9cfe44a55
Nishant Sharma <nishusharma1817@gmail.com>
2025-04-10 06:13:52 +0530

    update mutler

--- api/candidates.js
+++ api/candidates.js
@@ -11,7 +11,7 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "/tmp" });
 
 const candidateSchema = Joi.object({
   name: Joi.string().required(),
